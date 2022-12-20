2dd0d8a18ce21192bf26a76e353d6eed7296b998
ferfcb99 <fer_fcb99@outlook.com>
2022-12-19 20:40:52 -0600

    Se implementa swagger 3 u open api al proyecto

--- src/main/java/springcourse/api/entities/without_relations/People.java
+++ src/main/java/springcourse/api/entities/without_relations/People.java
@@ -3,15 +3,17 @@ package springcourse.api.entities.without_relations;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import org.springframework.data.annotation.CreatedDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 
 @Entity
 public class People implements Serializable{

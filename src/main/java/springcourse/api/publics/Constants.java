0c0ac827c702b760f63d041da997198fcd0f50be
ferfcb99 <fer_fcb99@outlook.com>
2022-12-21 20:53:33 -0600

    Se agregan constantes de mensajes de api

---
+++ src/main/java/springcourse/api/publics/Constants.java
@@ -0,0 +1,14 @@
+package springcourse.api.publics;
+
+public class Constants {
+	
+	public final static String GET_DATA_SUCCESSFULLY = "GET_DATA_SUCCESSFULLY";
+	public final static String GET_DATA_ERROR = "GET_DATA_ERROR";
+	public final static String CREATE_DATA_SUCCESSFULLY = "CREATE_DATA_SUCCESSFULLY";
+	public final static String CREATE_DATA_ERROR = "CREATE_DATA_ERROR";
+	public final static String UPDATE_DATA_SUCCESSFULLY = "UPDATE_DATA_SUCCESSFULLY";
+	public final static String UPDATE_DATA_ERROR = "UPDATE_DATA_ERROR";
+	public final static String DELETE_DATA_SUCCESSFULLY = "DELETE_DATA_SUCCESSFULLY";
+	public final static String DELETE_DATA_ERROR = "DELETE_DATA_ERROR";
+	
+}
